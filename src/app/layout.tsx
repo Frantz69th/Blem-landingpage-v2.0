02a83b909183a46380ff3897a2fda91c9a0a3121
Kuneism <kunaalpradhan58@gmail.com>
2024-04-12 16:04:16 +0530

    Landing Page

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import { ThemeProvider } from "@/providers/theme-provider"
+import "./globals.css";
+
+const font = DM_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Blem",
+  description: "#1 CRM for small businesses and growing businesses",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+        {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
